128a1fddcea5042e3f4ff8c80fed55817858fd00
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-07-08 14:45:17 +0500

    add work

--- chapter 38/script.js
+++ chapter 38/script.js
@@ -1,2 +1,10 @@
 /* Functions:
 Local vs. global variables */
+function even(number) {
+return number % 2 === 0 }
+console.log(even(6));
+
+function odd(number) {
+return number % 2 === 1 }
+console.log(odd(7));
+    
