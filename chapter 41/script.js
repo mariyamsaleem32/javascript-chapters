a3308eba5ceea5f22ec23ff94d9c7211caaaf6c6
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-07-10 15:22:32 +0500

    added examples

---
+++ chapter 41/script.js
@@ -0,0 +1,36 @@
+// let myNum = +prompt("Enter a number");
+// let i = 10;
+// while (i>=myNum) {
+//     console.log(i);
+//     i--;
+// }
+
+// let sum = 0;
+// let num = 1;
+// while (num<=100) {
+//     sum += num;     // sum = 0 + 1 = 1
+//     num++;
+// }
+// console.log(sum);
+
+// writ a program to display factorial of a number.
+// let number = 4;
+// let factorial = 1;
+// while (number > 0) {
+//     factorial = factorial * number;
+//     number --;
+// }
+// document.write("factorial of a number is:"+" "+factorial);
+
+// write a program to display finding the first 10 even number.
+ let evenNumber = [];
+ let i = 1;
+ while (evenNumber.length<10) {
+    if (i % 2 === 0) {
+        evenNumber.push(i)
+    } 
+    i++;
+    console.log(evenNumber);
+ }
+
+
