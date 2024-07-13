e97783b63dcc4121256961fbf9ff63484812a392
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-07-13 20:19:26 +0500

    update chapter 18

--- chapter 18/script.js
+++ chapter 18/script.js
@@ -1,22 +1,15 @@
 // for loop
 
-for (let i = 0; i < 6; i++) {
-console.log(i);    // loop will be upgrade 1 to 5
+for (var i = 0; i < 6; i++) {
+console.log(i); 
+break;   // loop will be upgrade 1 to 5
 }
-
-var word = prompt("Enter word");
-var reverse = "";
-
-for (let i = word.length - 1; i >= 0; i--) {
-     reverse += word[i];
-}
-
-if (word === reverse) {
-    console.log("It's a plindrome");
-}
-else{
-    console.log("It's not a plindrome");
-}
+// console.log(i);  
+  
+for (let i = 10; i >=5; i++) {
+    console.log(i); 
+    break;   // loop will not be upgrade answer is 0
+    }
 
 var arr = [11, 22, 83, 40, 55, 66, 78,10];
 var min = arr[0];
@@ -31,4 +24,15 @@ console.log(min);
 var pakistaniCities = "karachi";
 for (let i = 0; i < pakistaniCities.length; i++) {
     console.log(pakistaniCities[i]);
+}
+
+let word = prompt("Enter any word");
+let reverse = "";
+for (let i = word.length - 1; i >= 0; i--) {
+    reverse += word[i];
+}
+if (word === reverse) {
+    document.write("It's palindrom");   
+} else {
+    document.write("It's not palindrom");
 }
