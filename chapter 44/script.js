b9bc0b432836cbd4a07e7c1bc1b8b2b0c605166b
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-07-16 13:56:13 +0500

    add 3 more chapters

---
+++ chapter 44/script.js
@@ -0,0 +1,23 @@
+// Commenting 
+
+
+/* double line
+var arr = [11,13,14,15,19,20];
+var changing = [];
+{
+    for (let i = 0; i < arr.length - 1; i++) 
+ {
+    var diff = arr[i + 1]-arr[i];
+    if (diff > 1) 
+    {
+        for (let m = 1; m < diff; m++) 
+        {
+        changing.push(arr[i]+m);
+        }
+    }
+ }
+}
+console.log(arr);
+ console.log(changing); */
+
+ // single line
