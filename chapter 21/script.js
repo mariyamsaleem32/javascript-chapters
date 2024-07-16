c0a91487f0267f20c7749b5b898533823963455b
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-07-16 15:54:54 +0500

    add one example

--- chapter 21/script.js
+++ chapter 21/script.js
@@ -1,17 +1,21 @@
 // chapter 21 Changing case
 
-var cityToCheck = prompt("Enter your favorite city and I shall tell you it is clean or not");
-cityToCheck = cityToCheck.toLowerCase();
+// var cityToCheck = prompt("Enter your favorite city and I shall tell you it is clean or not");
+// cityToCheck = cityToCheck.toLowerCase();
 
-var cleanestCities = ["Cheyenne",  "santa fe", "Tucson", "great falls", "honolulu"];
+// var cleanestCities = ["Cheyenne",  "santa fe", "Tucson", "great falls", "honolulu"];
 
-for (let i = 0; i <  cleanestCities.length; i++) 
-    {
-        if(cityToCheck === cleanestCities[i])
-       {
-         alert("It's one of the clanest city");
-       }
-    }
+// for (let i = 0; i <  cleanestCities.length; i++) 
+//     {
+//         if(cityToCheck === cleanestCities[i])
+//        {
+//          alert("It's one of the clanest city");
+//        }
+//     }
 
 // Note that the keyword toLowerCase must be in camelCase.
+
+var fullName = "Mariyam Saleem";
+fullName = fullName.slice(0,1).toUpperCase();
+console.log(fullName);
 
