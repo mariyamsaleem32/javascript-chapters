18672b301b5d08bcd282029616a2397af390f79e
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-07-21 16:47:45 +0500

    add chapter 22

--- chapter 22/script.js
+++ chapter 22/script.js
@@ -4,3 +4,8 @@ and extracting part */
 
 var str = "hello world";
 console.log(str.length);
+console.log(str.slice(-5)); // negative index start from - 1
+console.log(str.slice(0,5)); //index 1 is 0 and and end index is extra 1
+console.log(str.slice(str.length-5)); 
+//complete length is 11 and 11 -5 is 6 space and world
+
---
+++ chapter 23/script.js
@@ -0,0 +1,3 @@
+/* Strings:
+Finding segments */
+
