8fb5d998519d69a55a9b9b9ff9bfc8da42e621dd
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-08-21 15:06:26 +0500

    add till chapter 60

---
+++ chapter 53/script.js
@@ -0,0 +1,12 @@
+// let on = document.getElementById("btnOn");
+// on.onclick="document.getElementById('myImage').src='blub off.jfif'";
+// let off = document.getElementById("btnOff");
+// off.onclick="document.getElementById('myImage').src='blub on.jfif'";
+
+function myFunction() {
+
+  document.getElementById("btnOn").onclick="document.getElementById('myImage').src='blub off.jfif'";
+
+ document.getElementById("btnOff").onclick="document.getElementById('myImage').src='blub on.jfif'";
+
+}
