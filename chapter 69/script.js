8048f570558105e02dfa0365ca15cd297e1ba1d1
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-08-26 15:02:24 +0500

    add till chapter 70

---
+++ chapter 69/script.js
@@ -0,0 +1,12 @@
+// Objects
+
+const student = {
+fullName : "ali",
+class : 4,
+rank : 2,
+section : "B" ,
+}
+console.log(student);
+
+student.fullName = "mariyam saleem";
+console.log(student.fullName);
