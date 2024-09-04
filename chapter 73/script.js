514389f3dfb91010a072796b835cb28cd0ff0fb8
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-09-04 15:52:23 +0500

    add more

--- chapter 73/script.js
+++ chapter 73/script.js
@@ -20,21 +20,27 @@ Constructors for methods */
 //     console.log(student2);
 //     console.log(student3);
 
-function student(name, fatherName, age, cls, email) {
-    this.name = name
-    this.fatherName = fatherName
-    this.age = age
-    this.cls = cls
-    this.email = email
-    this.nationality = "pakistani"
-};
+// let lastRecords = [ "mariyam saleem","hina salim","mariya ali","hamna umer","dua amjad","bisma tahir","umna aneef","sidra amir", ];
 
+// let checkrecord = prompt("enter patient name");
 
+// if (checkrecord = lastRecords) {
+//     this.lastRecord = "yes" 
+// }else {
+//     this.lastRecord = "no" 
+// }
 
-let student1 = new student("mariyam", "saleem", 18, "IV", "mariyam134556@gmail.com");
-let student2 = new student("hina", "saleem", 25, "x", "hinakhan139556@gmail.com");
-let student3 = new student("alina", "umer", 21, "xx", "alina661556@gmail.com");
+// function patient (fullName, age, dieses, wardNum, phNumber, lastRecord) {
+//     this.fullName = fullName
+//     this.age = age
+//     this.dieses  = dieses 
+//     this.wardNum = wardNum
+//     this.phNumber = phNumber
+//     this.lastRecord = lastRecord
+// };
 
-console.log(student1);
-console.log(student2);
-console.log(student3);
+// patient.prototype.nationality = "pakistani";
+
+// let patient1 = new patient("mariya ali", 18, "fever", 13, 12346770974, lastRecord);
+
+// console.log(patient1);
